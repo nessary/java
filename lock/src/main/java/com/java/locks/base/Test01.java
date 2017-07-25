b85aa9a8cc42a1d394870ba52b1ba22fc2d8e45e
nessary <nessary@foxmail.com>
2017-07-25 22:55:34 +0800

    更新锁

--- lock/src/main/java/com/java/locks/base/Test01.java
+++ lock/src/main/java/com/java/locks/base/Test01.java
@@ -1,6 +1,7 @@
 package com.java.locks.base;
 
 import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 
 public class Test01 {
@@ -17,77 +18,81 @@ public class Test01 {
         double c = 3d;
 
 
-        List<Person> list = new LinkedList<>();
-
-        Person person1 = new Person();
-        person1.setName("12");
-        person1.setAge(24);
-
-
-        list.add(person1);
-
-
-        Person person2 = new Person();
-        person2.setName("12");
-        person2.setAge(25);
-
-        list.add(person2);
-
-        Collections.sort(list);
-
-
-        list = list.stream().collect(Collectors.collectingAndThen(Collectors.toCollection(() -> new TreeSet<>((o1, o2) -> {
-            if (o2.getName().equals(o1.getName())) {
-
-
-                return 0;
-            } else {
-                return 1;
-            }
-        })), ArrayList::new))
-
-        ;
-        System.out.println(list);
+//        List<Person> list = new LinkedList<>();
+//
+//        Person person1 = new Person();
+//        person1.setName("12");
+//        person1.setAge(24);
+//
+//
+//        list.add(person1);
+//
+//
+//        Person person2 = new Person();
+//        person2.setName("12");
+//        person2.setAge(25);
+//
+//        list.add(person2);
+//
+//        //排序
+//        Collections.sort(list);
+//
+//
+//
+//        list = list.stream().collect(Collectors.collectingAndThen(Collectors.toCollection(() -> new TreeSet<>((o1, o2) -> {
+//            if (o2.getName().equals(o1.getName())) {
+//
+//
+//                return 0;
+//            } else {
+//                return 1;
+//            }
+//        })), ArrayList::new))
+//
+//        ;
+//        System.out.println(list);
 
 
     }
 
-    private static class Person implements Comparable<Person> {
-
-        private String name;
-
-        private Integer age;
-
-        public Integer getAge() {
-            return age;
-        }
-
-        public void setAge(Integer age) {
-            this.age = age;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        @Override
-        public String toString() {
-            return "Person{" +
-                    "name='" + name + '\'' +
-                    ", age=" + age +
-                    '}';
-        }
-
-        @Override
-        public int compareTo(Person o) {
-
-            //这里调节 需要的自定义相同排序的参数
-            return o.getAge() - this.getAge();
-        }
-    }
+//    private static class Person implements Comparable<Person> {
+//
+//        private String name;
+//
+//        private Integer age;
+//
+//        public Integer getAge() {
+//            return age;
+//        }
+//
+//        public void setAge(Integer age) {
+//            this.age = age;
+//        }
+//
+//        public String getName() {
+//            return name;
+//        }
+//
+//        public void setName(String name) {
+//            this.name = name;
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return "Person{" +
+//                    "name='" + name + '\'' +
+//                    ", age=" + age +
+//                    '}';
+//        }
+//
+//        @Override
+//        public int compareTo(Person o) {
+//
+//            CopyOnWriteArrayList
+//
+//            //这里调节 需要的自定义相同排序的参数
+//            return o.getAge() - this.getAge();
+//        }
+//    }
 
 }
---
+++ lock/src/main/java/com/java/locks/base/Test02.java
@@ -0,0 +1,147 @@
+package com.java.locks.base;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Test02 {
+
+
+    public static void main(String[] args) {
+
+        //happens-before(先天发生)
+        //-------------------------------------------------------------
+        //1.单核系统  所有的代码执行顺序都是按照 从上到下执行
+        long a = 3;
+        long b = 4;
+        long c = 5;
+        System.out.println(a + b + c);
+        //-------------------------------------------------------------
+
+
+        //-------------------------------------------------------------
+        ReentrantLock lock = new ReentrantLock();
+
+        Integer i = null;
+        try {
+            //before 条件
+            i += 1;
+            lock.lock();
+
+            //do something
+
+        } catch (Exception e) {
+
+        } finally {
+
+            if (lock.isLocked()) {
+                lock.unlock();
+            }
+
+
+        }
+
+
+        //lock 的
+        //-------------------------------------------------------------
+
+        //-------------------------------------------------------------
+
+        //3 volatile 的写操作 发生在读操作之前
+        //-------------------------------------------------------------
+
+
+        //-------------------------------------------------------------
+
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+
+        thread.start();
+
+        thread.interrupt();
+
+
+        //4 线程 start 发生在 任何操作之前
+        //-------------------------------------------------------------
+
+
+        //-------------------------------------------------------------
+
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("111");
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("222");
+            }
+        });
+
+
+        try {
+
+            thread1.start();
+            thread2.start();
+
+            thread1.join();
+            thread2.join();
+            System.out.println("333");
+        } catch (InterruptedException e) {
+
+        }
+
+
+        //4 线程  终止前所有操作全部结束
+        //-------------------------------------------------------------
+
+
+        //-------------------------------------------------------------
+
+
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("44444");
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                //do something
+
+
+                System.out.println("6666");
+
+
+            }
+        });
+
+        thread3.start();
+
+        thread3.interrupt();//中断
+
+
+        //4 线程  终止前所有操作全部结束
+        //-------------------------------------------------------------
+
+        ExecutorService service = Executors.newCachedThreadPool();
+        service.shutdown();
+        service.shutdownNow();
+
+
+    }
+
+
+}
